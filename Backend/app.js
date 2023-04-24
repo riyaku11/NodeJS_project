74f17bbac013fe373feef53f75ff7368359011ed
riyaku11 <riyaku217@gmail.com>
2023-04-24 14:33:43 +0530

    backend done

--- Backend/app.js
+++ Backend/app.js
@@ -19,27 +19,7 @@ res.status(201).json({
 
 })
 
-//1st api
-// app.get('/users/bmw-mercedes-income', async (req, res) => {
-//     const users = await Customer.match({
-//       income: { $lt: [ {$toDouble: { $substr: ["$income", 1, -1]}},5] },
-//       car: { $in: ['BMW', 'Mercedes-Benz'] }
-//     });
-//     res.json(users);
-//   });
-
-
-// app.get('/users/bmw-mercedes-income', async (req, res) => {
-//     const users = await Customer.find({
-//       $expr: {
-//         $and: [
-//           { $in: ['$car', ['BMW', 'Mercedes']] },
-//           { $lt: [{ $convert: { input: { $trim: { input: '$income', chars: '$' } }, to: 'decimal' } }, 5] }
-//         ]
-//       }
-//     });
-//     res.json(users);
-//   });
+//1st api: Users which have income lower than $5 USD and have a car of brand “BMW” or “Mercedes”.
 
 app.get('/users/bmw-mercedes-income', async (req, res) => {
     try {
@@ -70,34 +50,52 @@ app.get('/users/bmw-mercedes-income', async (req, res) => {
 
 
 
-//2nd api
+//2nd api:Male Users which have phone price greater than 10,000.
+
 app.get('/users/male-phone', async (req, res) => {
-    const users = await Customer.find({
-      gender: 'Male',
-      phone_price: { $gt: 10000 }
-    });
-    res.json(users);
-  });  
+  const users = await Customer.aggregate([
+    {
+      $match: {
+        gender: 'Male',
+        $expr: {
+          $gt: [{ $toDouble: "$phone_price" }, 10000]
+        }
+      }
+    }
+  ]);
+  res.json(users);
+});
+ 
 
-//3rd api
-app.get('/users/last-name-quote', async (req, res) => {
+//3rd api:Users whose last name starts with “M” and has a quote character length greater than 15 and email includes his/her last name
+app.get('/users/email-contains-lastname', async (req, res) => {
+  try {
     const users = await Customer.find({
+      $expr: { $regexMatch: { input: '$email', regex: '$last_name', options: 'i' } },
       last_name: /^M/,
-      quote: { $gt: 15 },
-      email: { $regex: `.*${user.last_name}.*` }
+      quote: { $gt: 15 }
     });
     res.json(users);
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal server error');
+  }
+});
 
-//4th api
+
+
+//4th api :Users which have a car of brand “BMW”, “Mercedes” or “Audi” and whose email does not include any digit.
 app.get('/users/brand-email', async (req, res) => {
     const users = await Customer.find({
-      car: { $in: ['BMW', 'Mercedes', 'Audi'] },
+      car: { $in: ['BMW', 'Mercedes-Benz', 'Audi'] },
       email: { $not: /\d/ }
     });
     res.json(users);
   });
 
+
+  
+  //5th api: Show the data of top 10 cities which have the highest number of users and their average income.
   app.get('/users/top-cities', async (req, res) => {
     const topCities = await Customer.aggregate([
       {
@@ -117,3 +115,6 @@ app.get('/users/brand-email', async (req, res) => {
 
 
 module.exports= app;
+
+
+
